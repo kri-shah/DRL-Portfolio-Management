93edaab5873bdebf6b2e4a3fdd3cde72642a2093
Krish Shah <krish.shah@uconn.edu>
2025-03-01 19:59:00 -0500

    data col

--- data_col.py
+++ data_col.py
@@ -1,7 +1,16 @@
 import yfinance as yf
 import pandas as pd
 
+def col_data(ticker):
+    t = yf.Ticker(ticker)
+    t_hist = t.history(period="max")
+    t_hist.to_csv(f'{ticker}.csv')
 
-ticker = yf.Ticker('XLE')
-aapl_historical = ticker.history(period="max")
-aapl_historical.to_csv('XLE.csv')
+
+def main():
+    sectors = ["XLC", "XLY", "XLP", "XLE", "XLF", "XLV", "XLI", "XLK", "XLB", "XLRE", "XLU"]
+    for sector in sectors:
+        col_data(sector)
+
+if __name__ == '__main__':
+    main()
